27be5b5861d54c1236ba2eb43ddd8b0e0868a5e3
harsh1111 <harsh.srivastava@zoopindia.com>
2024-03-04 03:22:00 +0530

    feat/video-component

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 // import ReactDOM from 'react-dom';
 
@@ -7,3 +8,38 @@ import App from './App'
 import { createRoot } from 'react-dom';
 
 createRoot(document.getElementById('root')).render(<App />);
+=======
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
+
+const io =require('socket.io')(server,{
+    cors : {
+        origin : '*',
+        methods : ['GET', 'POST']
+    }
+})
+
+app.use(cors());
+
+io.on('connection', (socket) =>{
+    socket.emit('me', socket.id);
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('callended');
+    })
+})
+socket.on("calluser", ({userToCall, signalData, from, name})=>{
+    io.to(userToCall).emit('calluser', {signal : signalData, from, name});
+});
+socket.on('answercall', (data)=>{
+    io.to(data.to).emit('callaccepted', data.signal);
+})
+const PORT = process.env.PORT || 3001;
+app.get('/', (req,res)=>{
+    res.send('Server is running');
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+>>>>>>> cd404bb3 (feat/videoComponent)
