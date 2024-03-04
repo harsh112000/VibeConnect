c85cab3dffec22888d605eca1cfd4787c7ea62ec
harsh1111 <harsh.srivastava@zoopindia.com>
2024-03-05 01:59:19 +0530

    final/commit

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
-<<<<<<< HEAD
-import React from 'react';
-// import ReactDOM from 'react-dom';
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
 
-import App from './App'
-
-// ReactDOM.render(<App/> , document.getElementById('root'));
-import { createRoot } from 'react-dom';
-
-createRoot(document.getElementById('root')).render(<App />);
-=======
-const app = require('express')();
-const server = require('http').createServer(app);
-const cors = require('cors');
-
-const io =require('socket.io')(server,{
-    cors : {
-        origin : '*',
-        methods : ['GET', 'POST']
-    }
-})
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
 
 app.use(cors());
 
-io.on('connection', (socket) =>{
-    socket.emit('me', socket.id);
-    socket.on('disconnect',()=>{
-        socket.broadcast.emit('callended');
-    })
-})
-socket.on("calluser", ({userToCall, signalData, from, name})=>{
-    io.to(userToCall).emit('calluser', {signal : signalData, from, name});
-});
-socket.on('answercall', (data)=>{
-    io.to(data.to).emit('callaccepted', data.signal);
-})
-const PORT = process.env.PORT || 3001;
-app.get('/', (req,res)=>{
-    res.send('Server is running');
-})
+const PORT = process.env.PORT || 5001;
 
-server.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
->>>>>>> cd404bb3 (feat/videoComponent)
+app.get('/', (req, res) => {
+	res.send('Running');
+});
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
